5d777946a677749ede58a089c3c5acac2110549f
2804whitehatJr <70639938+2804whitehatJr@users.noreply.github.com>
2020-11-07 11:58:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,87 +1,146 @@
-
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
 const Constraint = Matter.Constraint;
-var girl,ground,rock,tree,m1,m2,m3,m4,m5,m6,m7,chain;
 
-function preload()
-{
-	girl = loadImage("girl.png");
-	tree = loadImage("tree.png");
-	backgroundImg = loadImage("background.gif");
+var engine, world;
+var block1, block2, block3, block4, block5, block6, block7, block8, block9;
+var block10,block11,block12, block13, block14;
+var player;
+var ground1, ground2, ground3;
+
+var block15, block16, block17, block18, block19, block20, block21;
+var block22;
+var sling;
+var player;
+
+var ball;
+var img, background1;
+
+function preload(){
+  img = loadImage("ball.png");
+  background1= loadImage("background.gif")
 }
 
 function setup() {
-	createCanvas(1280, 400);
+  createCanvas(1000,500);
+  engine = Engine.create();
+    world = engine.world;
 
 
-	engine = Engine.create();
-	world = engine.world;
+  block1 = new Block(600,260,30,40);
+  block2 = new Block(570,260,30,40);
+  block3 = new Block(540,260,30,40);
+  block4 = new Block(630,260,30,40);
+  block5 = new Block(660,260,30,40);
 
-	//Create the Bodies Here.
-	rock = new stone(320,225);
-	chain = new SlingShot(rock.body,{x:320 , y:225});
-	m1 = new mango(900,150,8);
-	m2 = new mango(950,120,10);
-	m3 = new mango(1000,100,7);
-	m4 = new mango(950,60,9);
-	m5 = new mango(1050,60,6);
-	m6 = new mango(1100,120,10);
-	ground = Bodies.rectangle(640,385,1290,20,{isStatic:true});
-	World.add(world,ground);
-	
-	Engine.run(engine);
-	  
+
+
+  block6 = new Block(585,220,30,40);
+  block7 = new Block(555,220,30,40);
+  block8 = new Block(615,220,30,40);
+  block9 = new Block(645,220,30,40);
+
+
+
+  bolck10 = new Block(600,170,30,40);
+  block11 = new Block(570,180,30,40);
+  block12 = new Block(630,180,30,40);
+
+
+  block13 = new Block(600,140,30,40);
+
+  ground1 = new Ground(600,285,200,10);
+  ground2 = new Ground(900,195,200,10);
+  ground3 = new Ground(750, 600, 1500, 10)
+
+
+  block14 = new Block(900,170,30,40);
+  block15 = new Block(930,170,30,40);
+  block16 = new Block(870,170,30,40);
+  block17 = new Block(840,170,30,40);
+  block18 = new Block(960,170,30,40);
+
+  block19 = new Block(900,140,30,40);
+  block20 = new Block(930,140,30,40);
+  block21 = new Block(870,140,30,40);
+
+  block22 = new Block(900,110,30,40);
+
+  player = new Player(50,200,30,30);
+
+  ball = Bodies.circle(50,200,20);
+  World.add(world,ball);
+
+  sling = new Chain(this.ball,{x:150, y:160});
+
 }
-function mouseDragged(){
-    Matter.Body.setPosition(rock.body,{x:mouseX,y:mouseY});
-}
-function mouseReleased(){
-    chain.fly();
-}
-
 
 function draw() {
-	if (keyIsPressed === true) {
-		chain.attach();
-	  }
+  background(background1);
+  Engine.update(engine);
 
-    rectMode(CENTER);
-    background(backgroundImg);
-    push();
-    rect(width/2,400,width,20);
-	chain.display();
-    drawSprites();
-    console.log(rock);
-    imageMode(CENTER);
-    image(girl,370,200,180,220);
-	image(tree,1000,180,400,400);
-	rock.display();
-	m1.display();
-	m2.display();
-	m3.display();
-	m4.display();
-	m5.display();
-	m6.display();
-	collision(rock,m1);
-	collision(rock,m2);
-	collision(rock,m3);
-	collision(rock,m4);
-	collision(rock,m5);
-	collision(rock,m6);
+  fill(rgb(135, 205, 236));
 
-	strokeWeight(3);
-	stroke(0);
-	fill(255);
-	textSize(30);
-    text('Let us Pluck the Mangoes !😋', 285, 35);
-	
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+
+  fill("lightBlue");
+  block6.display();
+  block7.display();
+  block8.display();
+  block9.display();
+
+  fill("lightPink");
+  bolck10.display();
+  block11.display();
+  block12.display();
+  fill(rgb(47, 48, 48));
+
+  block13.display();
+
+  fill(rgb(135, 205, 236));
+  block14.display();
+  block15.display();
+  block16.display();
+  block17.display();
+  block18.display();
+  fill("lightGreen");
+
+  block19.display();
+  block20.display();
+  block21.display();
+  fill("lime");
+
+  block22.display();
+
+  ground1.display();
+  ground2.display();
+  ground3.display();
+
+  imageMode(CENTER);
+  image(img,ball.position.x,ball.position.y,40,40);
+
+  sling.display();
 }
-function collision(a,b){
-	var d = dist(a.body.position.x,a.body.position.y,b.body.position.x,b.body.position.y)
-	if(d <= 50){
-		Body.setStatic(b.body,false);
+
+
+function mouseDragged(){
+  Matter.Body.setPosition(this.ball, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+  sling.fly();
+}
+
+function keyPressed(){
+	if(keyCode === 32)
+	{
+		Matter.Body.setPosition(player.body,{x:235, y:420})
+		chain.attach(player.body);
 	}
 }
